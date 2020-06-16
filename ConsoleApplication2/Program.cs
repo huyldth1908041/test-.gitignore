0edff4ab6511cad594f5cec4c418964e3061b681
LuuHuy040501 <57887752+LuuHuy040501@users.noreply.github.com>
2020-06-16 18:32:12 +0700

    test

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test gitignore");
+        }
+    }
+}
